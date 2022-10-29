01db198cd4ad842db03cc5fe174f3d7d92770897
VladimirChukharev <Vladimir.Chukharev@gmail.com>
2022-10-29 16:55:49 +0400

    Merge changes made in SourceForge: more choices of data; better titles; remove old lines.

--- lagrange.py
+++ lagrange.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 #
-# https://hub.gke2.mybinder.org/user/numpy-numpy-tutorials-yne4ot5m/lab/tree/content/Untitled.ipynb
+# https://mybinder.org/v2/gh/VladimirChukharev/NumMethodsShorts/HEAD
 
 """Find Lagrange polynomial for given sets of points"""
 
@@ -13,21 +13,24 @@ import matplotlib.pyplot as plt
 
 def main() -> None:
     """Do the job"""
+    # x = np.array([-1,])
+    # x = np.array([-1, 0])
+    # x = np.array([-1, 0, 1])
+    # x = np.array([-1, 0, 1, 2])
     # x = np.array([0, 1, 2])
-    x = np.array([0, 1, 2, 1.5])
-    # y = x ** 3
-    # title = "$x^3$"
-    y = x ** 4
-    title = "$x^4$"
+    # x = np.array([0, 1, 2, 1.5])
+    x = np.array([-1, 0, 1, 2, 1.5])
+
+    y, title = x ** 3, "$x^3$"
+    # y, title = x ** 4, "$x^4$"
 
     poly = lagrange(x, y)
     coefficients = Polynomial(poly.coef[::-1]).coef
-    txt = f"${coefficients[0]} + " + " + ".join((f"{c} \\cdot x^{e + 1}" for e, c in enumerate(coefficients[1:]))) + "$"
-    x_new = np.arange(-0.1, 2.11, 0.1)
+    txt = (f"${coefficients[0]:g} + " +
+           " + ".join((f"{c:g} \\cdot x^{e + 1}" for e, c in enumerate(coefficients[1:]))) + "$")
+    x_new = np.arange(-1.1, 2.11, 0.1)
     plt.scatter(x, y, label='data')
     plt.plot(x_new, Polynomial(poly.coef[::-1])(x_new), label=txt)
-    plt.plot(x_new, 3 * x_new ** 2 - 2 * x_new + 0 * x_new,
-             label=r"$3 x^2 - 2 x$", linestyle='-.')
     plt.legend()
     plt.title(f"{title}: {list((xi, yi) for xi, yi in zip(x, y))}.")
     plt.show()
