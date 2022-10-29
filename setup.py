46d8752d3bef3f75605e2954e3f6fb66108ab8ab
VladimirChukharev <Vladimir.Chukharev@gmail.com>
2022-10-29 13:57:14 +0400

    Initial version of setup.py.

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""Configuration file for NumMethodShorts library"""
+
+from setuptools import setup
+
+setup(
+        name='NumMethodsShorts',
+        version='',
+        packages=[''],
+        url='',
+        license='',
+        author='VladimirChukharev',
+        author_email='',
+        description='Короткие программы для иллюстрации курса "Численные методы"'
+)
