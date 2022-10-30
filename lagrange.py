f5b34c10cc270bc1e29dc0cfcc0736f9e8e71d12
VladimirChukharev <Vladimir.Chukharev@gmail.com>
2022-10-30 15:25:58 +0400

    Type hints; plot linestyle; shorter comments.

--- lagrange.py
+++ lagrange.py
@@ -2,7 +2,8 @@
 # -*- coding: utf8 -*-
 #
 # https://mybinder.org/v2/gh/VladimirChukharev/NumMethodsShorts/HEAD
-# https://mybinder.org/v2/gh/VladimirChukharev/NumMethodsShorts/01db198cd4ad842db03cc5fe174f3d7d92770897?urlpath=lab%2Ftree%2Flagrange.ipynb
+# https://mybinder.org/v2/gh/VladimirChukharev/NumMethodsShorts/
+# 01db198cd4ad842db03cc5fe174f3d7d92770897?urlpath=lab%2Ftree%2Flagrange.ipynb
 
 """Find Lagrange polynomial for given sets of points"""
 
@@ -31,7 +32,7 @@ def main() -> None:
            " + ".join((f"{c:g} \\cdot x^{e + 1}" for e, c in enumerate(coefficients[1:]))) + "$")
     x_new = np.arange(-1.1, 2.11, 0.1)
     plt.scatter(x, y, label='data')
-    plt.plot(x_new, Polynomial(poly.coef[::-1])(x_new), label=txt)
+    plt.plot(x_new, Polynomial(poly.coef[::-1])(x_new), label=txt, linestyle="-.")
     plt.legend()
     plt.title(f"{title}: {list((xi, yi) for xi, yi in zip(x, y))}.")
     plt.show()
--- newton.py
+++ newton.py
@@ -2,7 +2,8 @@
 # -*- coding: utf8 -*-
 #
 # https://mybinder.org/v2/gh/VladimirChukharev/NumMethodsShorts/HEAD
-# https://mybinder.org/v2/gh/VladimirChukharev/NumMethodsShorts/01db198cd4ad842db03cc5fe174f3d7d92770897?urlpath=lab%2Ftree%2Flagrange.ipynb
+# https://mybinder.org/v2/gh/VladimirChukharev/NumMethodsShorts/
+# 01db198cd4ad842db03cc5fe174f3d7d92770897?urlpath=lab%2Ftree%2Flagrange.ipynb
 
 """
 Find Lagrange polynomial for given sets of points in Newton's form.
@@ -14,14 +15,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def _poly_newton_coefficient(x, y):
+def _poly_newton_coefficient(x: np.ndarray, y: np.ndarray) -> np.ndarray:
     """
     x: list or np array containing x data points
     y: list or np array containing y data points
     """
-    m = len(x)
+    m: int = len(x)
     # x = np.copy(x)
-    a = np.copy(y)
+    a: np.ndarray = np.copy(y)
     for k in range(1, m):
         print(a)
         a[k:m] = (a[k:m] - a[k - 1]) / (x[k:m] - x[k - 1])
@@ -29,16 +30,16 @@ def _poly_newton_coefficient(x, y):
     return a
 
 
-def newton_polynomial(x_data, y_data, x):
+def newton_polynomial(x_data: np.ndarray, y_data: np.ndarray, x: np.ndarray) -> np.ndarray:
     """
     x_data: data points at x
     y_data: data points at y
     x: evaluation point(s)
     """
-    a = _poly_newton_coefficient(x_data, y_data)
+    a: np.ndarray = _poly_newton_coefficient(x_data, y_data)
     print(f">>> {a}")
-    n = len(x_data) - 1  # Degree of polynomial
-    p = a[n]
+    n: int = len(x_data) - 1  # Degree of polynomial
+    p: np.ndarray = a[n]
     for k in range(1, n + 1):
         p = a[n - k] + (x - x_data[n - k]) * p
     return p
@@ -62,7 +63,7 @@ def main() -> None:
            " + ".join((f"{c:g} \\cdot x^{e + 1}" for e, c in enumerate(coefficients[1:]))) + "$")
     x_new = np.arange(-1.1, 2.11, 0.1)
     plt.scatter(x, y, label='data')
-    plt.plot(x_new, newton_polynomial(x, y, x_new), label=txt)
+    plt.plot(x_new, newton_polynomial(x, y, x_new), label=txt, linestyle=":")
     plt.legend()
     plt.title(f"{title}: {list((xi, yi) for xi, yi in zip(x, y))}.")
     plt.show()
