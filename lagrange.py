f561f07878d06e08d5e53df3f6f650aebeaf0397
VladimirChukharev <Vladimir.Chukharev@gmail.com>
2023-03-06 23:09:31 +0400

    Add one example more.

--- lagrange.py
+++ lagrange.py
@@ -22,7 +22,8 @@ def main() -> None:
     # x = np.array([0, 1, 2])
     # x = np.array([0, 1, 2, 1.5])
     # x = np.array([-1, 0, 1, 2, 1.5])
-    x = np.array([0.05, 0.1, 0.15, 0.2, 0.25])
+    # x, x_test = np.array([0.05, 0.1, 0.15, 0.2, 0.25]), 0.12
+    x, x_test = np.array([0.05, 0.1, 0.17, 0.25, 0.3]), 0.26
 
     # y, title = x ** 3, "$x^3$"
     # y, title = x ** 4, "$x^4$"
@@ -37,7 +38,7 @@ def main() -> None:
                 " + ".join((f"{c:.5g} \\cdot x^{e + 1}" for e, c in enumerate(coefficients[1:]))) + "$")
     # x_new = np.arange(-1.1, 2.11, 0.1)
     x_new = np.arange(-0.05, 0.35, 0.005)
-    plt.scatter(x, y, label=f"data {polynomial(0.12)=:.6g}")
+    plt.scatter(x, y, label=f"data {polynomial(x_test)=:.6g}")
     plt.plot(x_new, polynomial(x_new), label=txt, linestyle="-.")
     plt.legend()
     # plt.title(f"{title}: {''.join(str((xi, yi)) for xi, yi in zip(x, y))}.")
