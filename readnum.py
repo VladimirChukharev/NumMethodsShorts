3c5d0585cb47fa675318e724b37e28251fac606c
chukharev <chukharev@mail.ru>
2025-06-02 23:34:48 +0300

    Minor, but not PEP8 compliant...

--- readnum.py
+++ readnum.py
@@ -65,28 +65,16 @@ def char2int(character: str) -> int:
     """Convert a digit to an integer"""
     assert len(character) == 1
     match character:
-        case "0":
-            return 0
-        case "0":
-            return 0
-        case "1":
-            return 1
-        case "2":
-            return 2
-        case "3":
-            return 3
-        case "4":
-            return 4
-        case "5":
-            return 5
-        case "6":
-            return 6
-        case "7":
-            return 7
-        case "8":
-            return 8
-        case "9":
-            return 9
+        case "0": return 0
+        case "1": return 1
+        case "2": return 2
+        case "3": return 3
+        case "4": return 4
+        case "5": return 5
+        case "6": return 6
+        case "7": return 7
+        case "8": return 8
+        case "9": return 9
         case _:
             raise ValueError(f"Unexpected {character=}.")
 
