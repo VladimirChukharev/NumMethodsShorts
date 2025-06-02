f0a80c24e13eb6fd9d0033242719d955c479b8fe
chukharev <chukharev@mail.ru>
2025-06-02 16:42:39 +0300

    Finish conversion for q; rework some others; more test samples.

--- readnum.py
+++ readnum.py
@@ -2,6 +2,7 @@
 # -*- coding: utf8 -*-
 
 """Classify read numbers"""
+from math import gcd
 import re
 
 
@@ -60,62 +61,115 @@ class ReadNumber:
     indexes = ("sign", "zeros", "value", "dot", "mantissa", "periodic", "left", "right", "exp", "rest")
 
 
+def char2int(character: str) -> int:
+    """Convert a digit to an integer"""
+    assert len(character) == 1
+    match character:
+        case "0":
+            return 0
+        case "0":
+            return 0
+        case "1":
+            return 1
+        case "2":
+            return 2
+        case "3":
+            return 3
+        case "4":
+            return 4
+        case "5":
+            return 5
+        case "6":
+            return 6
+        case "7":
+            return 7
+        case "8":
+            return 8
+        case "9":
+            return 9
+        case _:
+            raise ValueError(f"Unexpected {character=}.")
+
+
+def str2natural(string: str) -> int:
+    """Convert a string of digits to a positive `int`"""
+    assert all((character in "0123456789" for character in string))
+
+    res: int = 0
+    for digit in string:
+        res = res * 10 + char2int(digit)
+    return res
+
+
 def natural_int(match_object: re.Match[str]) -> int:
     """Convert matched text to positive `int`"""
     assert match_object["sign"] in ("", "+") and match_object["value"][0] in "123456789"
-
-    res: int = 0
-    for digit in match_object["value"]:
-        res = res * 10 + int(digit)
-    return res
+    return str2natural(match_object["value"])
 
 
 def nonnegative(match_object: re.Match[str]) -> int:
     """Convert matched text to 0 or positive `int`"""
     assert match_object["sign"] in ("", "+")
-
-    res: int = 0
-    for digit in match_object["value"]:
-        res = res * 10 + int(digit)
-    return res
+    return str2natural(match_object["value"])
 
 
 def integer_int(match_object: re.Match[str]) -> int:
     """Convert matched text to `int`"""
     assert match_object["sign"] in ("", "+", "-")
-
-    res: int = 0
-    for digit in match_object["value"]:
-        res = res * 10 + int(digit)
-    return -res if match_object["sign"] == "-" else res
+    return -str2natural(match_object["value"]) if match_object["sign"] == "-" else str2natural(match_object["value"])
 
 
-def rational_num(match_object: re.Match[str]) -> tuple[int, int]:  # FIXME
+def rational_num(match_object: re.Match[str]) -> tuple[int, int]:
     """Convert matched text to a rational as a pair of int (second is unsigned but stored as signed)"""
     assert match_object["sign"] in ("", "+", "-")
 
-    res: int = 0
-    for digit in match_object["value"]:
-        res = res * 10 + int(digit)
-    return (-res, 1) if match_object["sign"] == "-" else (res, 1)
+    numerator: int = 0
+    denominator: int = 1
+    entire: int = str2natural(match_object["value"])
+    m_length: int = 0
+    if match_object["mantissa"] and (m_length := len(match_object["mantissa"])) > 0:
+        numerator = str2natural(match_object["mantissa"])
+        denominator = pow(10, m_length)
+    p_num: int = 0    # numerator for periodic part
+    p_denom: int = 1  # denominator for periodic part
+    if match_object["periodic"] and (p_length := len(match_object["periodic"])) > 0:
+        assert p_length > 2
+        p_length -= 2
+        p_num = str2natural(match_object["periodic"][1:-1])  # trim '(' and ')'
+        p_denom = (pow(10, p_length) - 1) * pow(10, m_length)
+    # print(f"{numerator=}, {denominator=}, {numerator/denominator=}; {p_num=}, {p_denom=}, {p_num/p_denom=}.")
+    divisor: int = 1
+    p_fact: int = p_denom
+    factor: int = denominator
+    if (divisor := gcd(denominator, p_denom)) > 1:
+        factor //= divisor
+        p_fact //= divisor
+    numerator = numerator * p_fact + p_num * factor
+    denominator *= p_fact
+    numerator += entire * denominator
+    if (divisor := gcd(denominator, numerator)) > 1:
+        denominator //= divisor
+        numerator //= divisor
+    return (-numerator, denominator) if match_object["sign"] == "-" else (numerator, denominator)
 
 
 def main() -> None:
     """Run a test"""
-    for text in "+0045", "-0045", "0.(3)", ".1e2", "0.1(9)", "12.", "-01.2(3)e-001", "0":
+    for text in "+0045", "-0045", "0.(3)", ".1e2", "0.1(9)", "12.", "-01.2(3)e-001", "0", "0.4", "-02.7(63)":
         for name, pattern in ReadNumber.patterns.items():
             match_object = pattern.match(text)
             if match_object and match_object['rest'] == '':
                 print(f"For {text=}: {name} =>> {match_object.groupdict()}.")
                 match name:
                     case "n":
-                        print(f"{natural_int(match_object)}")
+                        print(f"n:  {natural_int(match_object)}")
                     case "z0":
-                        print(f"{nonnegative(match_object)}")
+                        print(f"z0: {nonnegative(match_object)}")
                     case "z":
-                        print(f"{integer_int(match_object)}")
+                        print(f"z:  {integer_int(match_object)}")
                     case "q":
-                        print(f"{rational_num(match_object)}")
+                        nu, de = rational_num(match_object)
+                        print(f"q:  {nu}, {de}; {nu/de = }")
                     case _:
                         pass # raise ValueError(f"Unknown key {name=}.")
 
